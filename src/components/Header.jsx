42d4e822a319732aab1f00458124e5ccc1db1fb0
RazSherf <101274250+RazSherf@users.noreply.github.com>
2023-09-05 09:52:44 +0300

    fixing the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,14 @@ import { AppBar, Toolbar, Typography, IconButton, Link } from '@mui/material';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import StarRoundedIcon from '@mui/icons-material/StarRounded';
 import Switch from '@mui/material/Switch';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
 
 const Header = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Spinomenal Whether App
+        <Typography variant="h" component="div" sx={{ flexGrow: 1 ,fontFamily:'sans-serif' ,display:'flex',alignItems:'center',gap:1}}>
+          <WbSunnyIcon/> Whether App
         </Typography>
         <Link href="/" color="inherit" underline="none" style={{ marginRight: 16 }}>
           <HomeRoundedIcon />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import style from '../index.css'
+import { useState } from 'react'
+import '../pages/home.css'
 import { TextField } from '@mui/material';
 
 const Home = () => {
+  const [citySearch, setCitySearch] = useState('')
+
+  const handleChange = (e) => {
+    setCitySearch(e.target.value)
+
+  }
+
   return (
-    <div className={style.container}>
-        <TextField id="outlined-basic" label="Search City" variant="outlined"/>
+    <div className="container">
+      <TextField id="outlined-basic" label="Search City" variant="outlined" onChange={handleChange} />
+      {citySearch}
     </div>
   )
 }
