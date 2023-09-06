c2373d2906165338dba3fd5d5e43cdb5c97d41f3
RazSherf <101274250+RazSherf@users.noreply.github.com>
2023-09-06 11:18:00 +0300

    Adding default location and add to favorite button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
-import { Autocomplete, Box, TextField } from '@mui/material';
+import { Autocomplete, Box, Fab, TextField } from '@mui/material';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import DailyWeatherInfo from '../components/DailyWeatherInfo/DailyWeatherInfo';
 import '../pages/home.css';
-
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
 
 const locationsArray = [
   {
@@ -99,125 +100,187 @@ const locationsArray = [
 ];
 
 const forecasts = [
-    {
-      Date: '2023-09-06T07:00:00+03:00',
-      EpochDate: 1693972800,
-      Temperature: {
-        Minimum: { Value: 27.4, Unit: 'C', UnitType: 17 },
-        Maximum: { Value: 33.9, Unit: 'C', UnitType: 17 }
-      },
-      Day: {
-        Icon: 1,
-        IconPhrase: 'Sunny',
-        HasPrecipitation: false
-      },
-      Night: {
-        Icon: 33,
-        IconPhrase: 'Clear',
-        HasPrecipitation: false
-      },
-      Sources: ['AccuWeather'],
-      MobileLink:
-        'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=1&unit=c&lang=en-us',
-      Link:
-        'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=1&unit=c&lang=en-us'
+  {
+    Date: '2023-09-06T07:00:00+03:00',
+    EpochDate: 1693972800,
+    Temperature: {
+      Minimum: { Value: 27.4, Unit: 'C', UnitType: 17 },
+      Maximum: { Value: 33.9, Unit: 'C', UnitType: 17 }
     },
-    {
-      Date: '2023-09-07T07:00:00+03:00',
-      EpochDate: 1694059200,
-      Temperature: {
-        Minimum: { Value: 27.3, Unit: 'C', UnitType: 17 },
-        Maximum: { Value: 33.1, Unit: 'C', UnitType: 17 }
-      },
-      Day: {
-        Icon: 1,
-        IconPhrase: 'Sunny',
-        HasPrecipitation: false
-      },
-      Night: {
-        Icon: 33,
-        IconPhrase: 'Clear',
-        HasPrecipitation: false
-      },
-      Sources: ['AccuWeather'],
-      MobileLink:
-        'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=2&unit=c&lang=en-us',
-      Link:
-        'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=2&unit=c&lang=en-us'
+    Day: {
+      Icon: 1,
+      IconPhrase: 'Sunny',
+      HasPrecipitation: false
     },
-    {
-      Date: '2023-09-08T07:00:00+03:00',
-      EpochDate: 1694145600,
-      Temperature: {
-        Minimum: { Value: 28.9, Unit: 'C', UnitType: 17 },
-        Maximum: { Value: 33.7, Unit: 'C', UnitType: 17 }
-      },
-      Day: {
-        Icon: 1,
-        IconPhrase: 'Sunny',
-        HasPrecipitation: false
-      },
-      Night: {
-        Icon: 33,
-        IconPhrase: 'Clear',
-        HasPrecipitation: false
-      },
-      Sources: ['AccuWeather'],
-      MobileLink:
-        'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=3&unit=c&lang=en-us',
-      Link:
-        'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=3&unit=c&lang=en-us'
+    Night: {
+      Icon: 33,
+      IconPhrase: 'Clear',
+      HasPrecipitation: false
     },
-    {
-      Date: '2023-09-09T07:00:00+03:00',
-      EpochDate: 1694232000,
-      Temperature: {
-        Minimum: { Value: 27, Unit: 'C', UnitType: 17 },
-        Maximum: { Value: 33.8, Unit: 'C', UnitType: 17 }
-      },
-      Day: {
-        Icon: 1,
-        IconPhrase: 'Sunny',
-        HasPrecipitation: false
-      },
-      Night: {
-        Icon: 38,
-        IconPhrase: 'Mostly cloudy',
-        HasPrecipitation: false
-      },
-      Sources: ['AccuWeather'],
-      MobileLink:
-        'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=4&unit=c&lang=en-us',
-      Link:
-        'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=4&unit=c&lang=en-us'
+    Sources: ['AccuWeather'],
+    MobileLink:
+      'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=1&unit=c&lang=en-us',
+    Link:
+      'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=1&unit=c&lang=en-us'
+  },
+  {
+    Date: '2023-09-07T07:00:00+03:00',
+    EpochDate: 1694059200,
+    Temperature: {
+      Minimum: { Value: 27.3, Unit: 'C', UnitType: 17 },
+      Maximum: { Value: 33.1, Unit: 'C', UnitType: 17 }
     },
-    {
-      Date: '2023-09-10T07:00:00+03:00',
-      EpochDate: 1694318400,
-      Temperature: {
-        Minimum: { Value: 26, Unit: 'C', UnitType: 17 },
-        Maximum: { Value: 30.7, Unit: 'C', UnitType: 17 }
+    Day: {
+      Icon: 1,
+      IconPhrase: 'Sunny',
+      HasPrecipitation: false
+    },
+    Night: {
+      Icon: 33,
+      IconPhrase: 'Clear',
+      HasPrecipitation: false
+    },
+    Sources: ['AccuWeather'],
+    MobileLink:
+      'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=2&unit=c&lang=en-us',
+    Link:
+      'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=2&unit=c&lang=en-us'
+  },
+  {
+    Date: '2023-09-08T07:00:00+03:00',
+    EpochDate: 1694145600,
+    Temperature: {
+      Minimum: { Value: 28.9, Unit: 'C', UnitType: 17 },
+      Maximum: { Value: 33.7, Unit: 'C', UnitType: 17 }
+    },
+    Day: {
+      Icon: 1,
+      IconPhrase: 'Sunny',
+      HasPrecipitation: false
+    },
+    Night: {
+      Icon: 33,
+      IconPhrase: 'Clear',
+      HasPrecipitation: false
+    },
+    Sources: ['AccuWeather'],
+    MobileLink:
+      'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=3&unit=c&lang=en-us',
+    Link:
+      'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=3&unit=c&lang=en-us'
+  },
+  {
+    Date: '2023-09-09T07:00:00+03:00',
+    EpochDate: 1694232000,
+    Temperature: {
+      Minimum: { Value: 27, Unit: 'C', UnitType: 17 },
+      Maximum: { Value: 33.8, Unit: 'C', UnitType: 17 }
+    },
+    Day: {
+      Icon: 1,
+      IconPhrase: 'Sunny',
+      HasPrecipitation: false
+    },
+    Night: {
+      Icon: 38,
+      IconPhrase: 'Mostly cloudy',
+      HasPrecipitation: false
+    },
+    Sources: ['AccuWeather'],
+    MobileLink:
+      'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=4&unit=c&lang=en-us',
+    Link:
+      'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=4&unit=c&lang=en-us'
+  },
+  {
+    Date: '2023-09-10T07:00:00+03:00',
+    EpochDate: 1694318400,
+    Temperature: {
+      Minimum: { Value: 26, Unit: 'C', UnitType: 17 },
+      Maximum: { Value: 30.7, Unit: 'C', UnitType: 17 }
+    },
+    Day: {
+      Icon: 2,
+      IconPhrase: 'Mostly sunny',
+      HasPrecipitation: false
+    },
+    Night: {
+      Icon: 34,
+      IconPhrase: 'Mostly clear',
+      HasPrecipitation: false
+    },
+    Sources: ['AccuWeather'],
+    MobileLink:
+      'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=5&unit=c&lang=en-us',
+    Link:
+      'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=5&unit=c&lang=en-us'
+  }
+];
+
+const telAvivLocation = {
+  "Version": 1,
+  "Key": "215854",
+  "Type": "City",
+  "Rank": 31,
+  "LocalizedName": "Tel Aviv",
+  "EnglishName": "Tel Aviv",
+  "PrimaryPostalCode": "",
+  "Region": {
+    "ID": "MEA",
+    "LocalizedName": "Middle East",
+    "EnglishName": "Middle East"
+  },
+  "Country": {
+    "ID": "IL",
+    "LocalizedName": "Israel",
+    "EnglishName": "Israel"
+  },
+  "AdministrativeArea": {
+    "ID": "TA",
+    "LocalizedName": "Tel Aviv",
+    "EnglishName": "Tel Aviv",
+    "Level": 1,
+    "LocalizedType": "District",
+    "EnglishType": "District",
+    "CountryID": "IL"
+  },
+  "TimeZone": {
+    "Code": "IDT",
+    "Name": "Asia/Jerusalem",
+    "GmtOffset": 3,
+    "IsDaylightSaving": true,
+    "NextOffsetChange": "2023-10-28T23:00:00Z"
+  },
+  "GeoPosition": {
+    "Latitude": 32.045,
+    "Longitude": 34.77,
+    "Elevation": {
+      "Metric": {
+        "Value": 34,
+        "Unit": "m",
+        "UnitType": 5
       },
-      Day: {
-        Icon: 2,
-        IconPhrase: 'Mostly sunny',
-        HasPrecipitation: false
-      },
-      Night: {
-        Icon: 34,
-        IconPhrase: 'Mostly clear',
-        HasPrecipitation: false
-      },
-      Sources: ['AccuWeather'],
-      MobileLink:
-        'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=5&unit=c&lang=en-us',
-      Link:
-        'http://www.accuweather.com/en/il/tel-aviv/215854/daily-weather-forecast/215854?day=5&unit=c&lang=en-us'
+      "Imperial": {
+        "Value": 111,
+        "Unit": "ft",
+        "UnitType": 0
+      }
     }
-  ];
+  },
+  "IsAlias": false,
+  "SupplementalAdminAreas": [],
+  "DataSets": [
+    "AirQualityCurrentConditions",
+    "AirQualityForecasts",
+    "Alerts",
+    "DailyPollenForecast",
+    "ForecastConfidence",
+    "FutureRadar",
+    "MinuteCast"
+  ]
+};
 
 const Home = () => {
-
   const [citySearch, setCitySearch] = useState('')
   const [selectedCity, setSelectedCity] = useState(null);
   const [fiveDayForecasts, setFiveDayForecasts] = useState([])
@@ -230,9 +293,24 @@ const Home = () => {
   }
   const handleAutoCompleteChange = (event, newValue) => {
     console.log(newValue);
-    // setCitySearch(newValue)
     setSelectedCity(newValue);
   }
+
+  const fetchDefaultLocation = async () => {
+    try {
+      // const locationKeyUrl = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=TelAviv`;
+      // const response = await axios.get(locationKeyUrl);
+      // const data = await response.data;
+      // setSelectedCity(data[0]);
+      setSelectedCity(telAvivLocation);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    fetchDefaultLocation();
+  }, []);
 
   useEffect(() => {
 
@@ -252,7 +330,6 @@ const Home = () => {
       // const apiUrl = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${apiKey}&metric=true`;
       // const response = await axios.get(apiUrl);
       // const data = await response.data;
-      // console.log('data', data);
       // setFiveDayForecasts(data.DailyForecasts);
       setFiveDayForecasts(forecasts);
     } catch (e) {
@@ -262,7 +339,7 @@ const Home = () => {
 
   const fetchWithAutoComplete = async (citySearch) => {
     try {
-      // const apiUrl = `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=eyvGn4yVUxjAxUUXRtMTp9UfmOxboAyt&q=${citySearch}`
+      // const apiUrl = `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${citySearch}`
       // const response = await axios.get(apiUrl);
       // const data = await response.data;
       // console.log(data);
@@ -271,7 +348,6 @@ const Home = () => {
       // })
       const localizedOptions = locationsArray;
       setLocalized(localizedOptions)
-      // console.log(data[]);
     } catch (e) {
       console.log(e);
     }
@@ -297,15 +373,30 @@ const Home = () => {
         </Box>
       </Box>
       {selectedCity && (
-        <div className='forecastsContainer'>
-          {fiveDayForecasts.map((dayForecast) => (
-            <DailyWeatherInfo
-              key={dayForecast.Date}
-              day={dayForecast.Date}
-              temperature={dayForecast.Temperature.Maximum.Value}
-              unit={dayForecast.Temperature.Maximum.Unit}
-              weatherCondition={dayForecast.Day.IconPhrase} />
-          ))}
+        <div>
+          <div className='selectedCityHeader'>
+            {selectedCity.LocalizedName}
+
+            <Fab color="primary" aria-label="add" size="small" sx={{
+              backgroundColor: 'red', ":hover": {
+                backgroundColor: 'red',
+              }
+            }} className='likeButton' onClick={() => alert('add to fav')}>
+              <FavoriteBorderOutlinedIcon />
+            </Fab>
+            {/* <FavoriteBorderOutlinedIcon /> */}
+            {/* <FavoriteOutlinedIcon sx={{color: 'red'}} /> */}
+          </div>
+          <div className='forecastsContainer'>
+            {fiveDayForecasts.map((dayForecast) => (
+              <DailyWeatherInfo
+                key={dayForecast.Date}
+                day={dayForecast.Date}
+                temperature={dayForecast.Temperature.Maximum.Value}
+                unit={dayForecast.Temperature.Maximum.Unit}
+                weatherCondition={dayForecast.Day.IconPhrase} />
+            ))}
+          </div>
         </div>
       )}
     </div>
